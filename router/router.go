94e4999b579a5129bd3fbe348cad904801f4085b
Rick Bassham <brodrick.bassham@gmail.com>
2018-05-22 17:34:13 -0400

    Add StaticFiles and CORS support.

--- router/router.go
+++ router/router.go
@@ -2,11 +2,15 @@ package router
 
 import (
 	"net/http"
+	"strings"
+
+	"github.com/spf13/afero"
 
 	"github.com/didip/tollbooth"
 	"github.com/didip/tollbooth_chi"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 
 	m "github.com/rickbassham/goapi/middleware"
 )
@@ -23,6 +27,7 @@ type RouteCreater interface {
 
 type Router interface {
 	ListenAndServe() error
+	StaticFiles(path string, root afero.Fs)
 }
 
 func NewRouter(address string, logger m.RequestLogger, routeCreater RouteCreater) Router {
@@ -39,6 +44,18 @@ func NewRouter(address string, logger m.RequestLogger, routeCreater RouteCreater
 	})
 
 	r.Group(func(r chi.Router) {
+		cors := cors.New(cors.Options{
+			// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+			AllowedOrigins: []string{"*"},
+			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+			AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+			ExposedHeaders:   []string{"Link"},
+			AllowCredentials: true,
+			MaxAge:           300, // Maximum value not ignored by any of major browsers
+		})
+		r.Use(cors.Handler)
+
 		r.Use(middleware.RequestID)
 		r.Use(middleware.RealIP)
 		r.Use(middleware.DefaultCompress)
@@ -59,6 +76,25 @@ func NewRouter(address string, logger m.RequestLogger, routeCreater RouteCreater
 		r:       r,
 		logger:  logger,
 	}
+}
+
+func (svc *router) StaticFiles(path string, root afero.Fs) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	httpFs := afero.NewHttpFs(root)
+	fs := http.StripPrefix(path, http.FileServer(httpFs.Dir("/")))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		svc.r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	svc.r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
 
 func (svc *router) ListenAndServe() error {
